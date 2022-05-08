d1e25b7820ac4a1bdb96d170261830379beebb71
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-05-08 17:25:27 -0400

    done

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState, useEffect} from "react";
 
-function ListingsContainer() {
+import ListingCard from "./ListingCard";
+
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/listings')
+    .then((r)=> r.json())
+    .then((items)=>setListings(items))
+  },[])
+
+
+
+function handleDeleteListing(id){
+const updatedListingsArray = listings.filter(listing => listing.id !== id)
+setListings(updatedListingsArray)
+}
+
+const filteredListings = listings.filter(listing => {
+  return listing.description.toLowerCase().includes(search.toLowerCase())
+})
+
+  const listingCards = filteredListings.map((listingObj) => {
+    return <ListingCard 
+    key={listingObj.id} 
+    listing={listingObj}
+    onDeleteListing={handleDeleteListing} />
+  })
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({ onSearch }) {
+  const [currentSearch, setCurrentSearch] = useState("")
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+   onSearch(currentSearch);
   }
 
   return (
@@ -13,7 +15,7 @@ function Search() {
         id="search"
         placeholder="search free stuff"
         value={""}
-        onChange={(e) => console.log(e.target.value)}
+        onChange={(e) => setCurrentSearch(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
